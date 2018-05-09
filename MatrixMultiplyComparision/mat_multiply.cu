9fe8bc48c6088a2e855655115e6d9c3352efb42d
utp <utp@utp>
2018-05-09 12:06:22 -0500

    fix bugs

--- MatrixMultiplyComparision/mat_multiply.cu
+++ MatrixMultiplyComparision/mat_multiply.cu
@@ -45,19 +45,19 @@ void MatrixMultiplySMKernel(float *d_A, float *d_B, float *d_R, int colsA, int r
 
 	float Pvalue = 0;
 	for (int m = 0; m < (TILE_WIDTH + colsA - 1)/TILE_WIDTH; ++m){
-		
+
 		if(m * TILE_WIDTH + tx < colsA && row < rowsA){
-			Ads[ty][tx] = d_A[row * colsA + k * TILE_WIDTH + tx];
+			Ads[ty][tx] = d_A[row * colsA + m * TILE_WIDTH + tx];
 		}
 		else{
-			Ads[ty][tx] = 0.0;	
+			Ads[ty][tx] = 0.0;
 		}
 
 		if(m * TILE_WIDTH + ty < rowsB && col < colsB){
 			Bds[ty][tx] = d_B[(m * TILE_WIDTH + ty) * colsB + col];
 		}
 		else{
-			Bds[ty][tx] = 0.0;	
+			Bds[ty][tx] = 0.0;
 		}
 
 		__syncthreads();
